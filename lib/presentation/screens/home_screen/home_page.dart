31a25b3201e23efe831dfca23528fef27bb0bd5f
kasun <kasundularaam@gmail.com>
2022-07-08 09:30:27 +0530

    home tabs added

--- lib/presentation/screens/home_screen/home_page.dart
+++ lib/presentation/screens/home_screen/home_page.dart
@@ -1,11 +1,11 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../../../core/themes/app_colors.dart';
+import '../../../data/models/app_user.dart';
+import 'tabs/home_tab.dart';
+import 'tabs/profile_tab.dart';
 
-import 'package:flutter3_startup_project/data/models/app_user.dart';
-
-import '../../../core/components/components.dart';
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final AppUser appUser;
   const HomePage({
     Key? key,
@@ -13,9 +13,60 @@ class HomePage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static final List<Widget> _widgetOptions = [
+    const HomeTab(),
+    const ProfileTab(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: textP("Home Page", 14)),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: AppColors.primaryColor,
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        backgroundColor: AppColors.primaryColor,
+        body: SafeArea(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_rounded,
+              ),
+              label: "Home",
+              backgroundColor: AppColors.primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_rounded,
+              ),
+              label: "Profile",
+              backgroundColor: AppColors.primaryColor,
+            ),
+          ],
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedIndex,
+          selectedItemColor: AppColors.lightElv0,
+          unselectedItemColor: AppColors.lightElv0.withOpacity(0.7),
+          iconSize: 40,
+          onTap: _onItemTapped,
+          elevation: 5,
+        ),
+      ),
     );
   }
 }
